812231af756f840f4a6a36ca5c567d1485d5a1c2
GizziXZ <bunclesgoodstuff@gmail.com>
2024-08-29 15:57:58 +0300

    broken postpage, trying to fix

--- frontend/src/components/PostPage.js
+++ frontend/src/components/PostPage.js
@@ -6,6 +6,10 @@ import styles from './PostPage.module.css';
 const PostPage = () => {
   const { id } = useParams();
   const [post, setPost] = useState(null);
+  const [isActive, setIsActive] = useState('');
+  const [isSpotifyLoaded, setIsSpotifyLoaded] = useState(false);
+    const [spotifyController, setSpotifyController] = useState(null);
+  const [IFrameAPI, setIFrameAPI] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -21,26 +25,55 @@ const PostPage = () => {
     fetchPost();
   }, [id]);
 
-  if (!post) {
-    return <div>Loading...</div>;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://open.spotify.com/embed/iframe-api/v1';
+    script.async = true;
+    script.onload = () => {
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        setIFrameAPI(IFrameAPI);
+        const spotifyIframe = document.querySelector('iframe[src*="spotify.com/embed"]');
+        if (spotifyIframe) {
+          IFrameAPI.createController(spotifyIframe, (controller) => {
+            setSpotifyController(controller);
+            setIsSpotifyLoaded(true);
+          });
+        }
+      };
+    };
+    document.body.appendChild(script);
+  }, [post]);
+
+  if (!post) { // loading screen
+    return <div></div>;
   }
 
   const imageUrl = `data:${post.image.mimetype};base64,${post.image.buffer}`;
 
-    // Regex pattern to match Spotify track links
     const spotifyPattern = /https:\/\/open.spotify.com\/track\/([a-zA-Z0-9]+)(\?si=[a-zA-Z0-9]+)?/g;
 
-    // Replace Spotify link with an iframe
     const spotifyEmbed = post.spotifyLink.replace(
       spotifyPattern,
-      '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/$1?autoplay=true" width="80%" height="152" frameborder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture;" loading="lazy"></iframe>'
+      '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/$1#0:39" width="80%" height="152" frameborder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture;" loading="lazy"></iframe>'
     );
+
+    const handleClick = () => {
+        if (isSpotifyLoaded) {
+            const spotifyEmbedIframe = document.querySelector('iframe[src*="spotify.com/embed"]');
+            const spotifyEmbedWindow = spotifyEmbedIframe.contentWindow;
+            // spotifyEmbedWindow.postMessage({ command: 'resume' }, '*');
+            IFrameAPI.createController(spotifyEmbedIframe, (controller) => {
+                controller.play();
+            });
+            setIsActive(true);
+        }
+    };
 
   return (
     <div>
     <Header />
     <div className={styles.box}>
-        <div className={styles.container}>
+        <div className={`${styles.container} ${isActive ? styles.active : ''}`} onClick={handleClick}>
         <div className={styles.header}>
             <img className={styles.image} src={imageUrl} alt={post.title} />
             <h1 className={styles.title}>{post.title}</h1>
